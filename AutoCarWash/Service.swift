9eb4af613361090e55909eb12940ed38b0211cbf
tonarisun <pozitive@inbox.ru>
2019-08-05 13:54:07 +0700

    Registration, save User in Realm

---
+++ AutoCarWash/Service.swift
@@ -0,0 +1,27 @@
+//
+//  Service.swift
+//  AutoCarWash
+//
+//  Created by Olga Lidman on 05/08/2019.
+//  Copyright © 2019 Home. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Service {
+    
+    func saveDataInRealmWithDeletingOld(object: Object, objectType: Object.Type){
+        do {
+            let realm = try Realm()
+            let oldObject = realm.objects(objectType)
+            realm.beginWrite()
+            realm.delete(oldObject)
+            realm.add(object)
+            try realm.commitWrite()
+        } catch {
+            print(error)
+        }
+    }
+    
+}
